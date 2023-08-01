c03320ccdf8af483a5a945fb02ab4a5b8f080860
riichi <88538677+riichimaru@users.noreply.github.com>
2023-08-02 00:03:50 +0200

    Update time.ts

--- src/pages/api/time.ts
+++ src/pages/api/time.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=51.408169&lng=-0.025813&formatted=0`
+    `https://api.sunrise-sunset.org/json?lat=54.608516&lng=20.228902&formatted=0`
   ).then((res) => res.json());
 
   res.status(200).json(response);
