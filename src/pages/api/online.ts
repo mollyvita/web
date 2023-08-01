b3e9814a3533ec4b70dcb398aadb56efc5ae9427
riichi <88538677+riichimaru@users.noreply.github.com>
2023-08-02 00:07:57 +0200

    Update online.ts

--- src/pages/api/online.ts
+++ src/pages/api/online.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `https://api.lanyard.rest/v1/users/798929927318536263`
+    `https://api.lanyard.rest/v1/users/920329543187832912`
   ).then((res) => res.json());
 
   res.status(200).json(response);
