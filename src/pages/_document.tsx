b65a6b569256b76aea4eda1415e03a5eedb16707
riichi <88538677+riichimaru@users.noreply.github.com>
2023-08-02 00:11:45 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class WebsiteDocument extends Document {
 		return (
 			<Html>
 				<Head>
-				<link rel="icon" type="image/png" href="https://static.tectrainguy.xyz/in-mac.png" />
+				<link rel="icon" type="image/png" href="https://cdn.discordapp.com/avatars/330299657458679821/76a60c9f0a3f0e51829507026e22e962.png?size=4096" />
 				</Head>
 				<body className="bg-[#f7f2f2] dark:bg-zinc-800">
 					<Main />
